ee5af00b11187522186fe21ef088600b7f0faab5
Samperfect <lovedayperfection1@gmail.com>
2021-05-15 20:01:54 +0100

    modified the user dashboard

--- abbrefy/users/routes.py
+++ abbrefy/users/routes.py
@@ -73,4 +73,4 @@ def dashboard(username):
     site_title = "Abbrefy | Grow As You Abbrefy"
     links = User.my_links(session['current_user'])
 
-    return render_template('dashboard.html', datetime=datetime, site_title=site_title, links=links)
+    return render_template('dashboard.html', datetime=datetime, site_title=site_title, links=links, len=len)
