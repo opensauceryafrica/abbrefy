b7b4154146878a501798be1f5500816564e0c47f
Perfection Loveday <lovedayperfection1@gmail.com>
2021-05-18 20:15:29 +0100

    implemented links tabs

--- abbrefy/static/js/dashboard.js
+++ abbrefy/static/js/dashboard.js
@@ -1,8 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.bitlink-item--MAIN').classList =
     'bitlink-item--ACTIVE';
+
+  document.querySelector('.item-detail--created-date').textContent =
+    'CREATED ' +
+    document.querySelector('.bitlink-item--created-date').dataset.date_created;
+
+  document.querySelector('.item-detail--title').textContent =
+    document.querySelector('.bitlink-item--title').dataset.title;
+
+  document.querySelector('.item-detail--url').textContent =
+    document.querySelector('.bitlink--MAIN').dataset.origin;
+
+  document
+    .querySelector('.item-detail--url')
+    .setAttribute(
+      'href',
+      document.querySelector('.bitlink--MAIN').dataset.origin
+    );
+
+  document.querySelector('.button--COPY').dataset.clipboard =
+    document.querySelector('.bitlink--MAIN').dataset.abbrefy;
+
+  document.querySelector('.bitlink--detail--MAIN').textContent =
+    document.querySelector('.bitlink--MAIN').dataset.abbrefy;
+
+  document
+    .querySelector('.bitlink--copy-tooltip')
+    .querySelector('input').value =
+    document.querySelector('.bitlink--MAIN').dataset.abbrefy;
+
+  document
+    .querySelector('.bitlink--detail--MAIN')
+    .setAttribute(
+      'title',
+      document.querySelector('.bitlink--MAIN').dataset.abbrefy
+    );
+
+  document
+    .querySelector('.bitlink--copyable-text')
+    .setAttribute(
+      'href',
+      document.querySelector('.bitlink--MAIN').dataset.abbrefy
+    );
+
+  document.querySelector('.info-wrapper--clicks-text').textContent =
+    document.querySelector('.click-count--MAIN').dataset.clicks;
+
+  locations = [
+    ...document
+      .querySelector('.click-count--MAIN')
+      .dataset.audience.split('[')[1]
+      .split(']')[0]
+      .split(', '),
+  ];
+
+  locations.forEach((location) => {
+    document.querySelector('.audience--locations---MAIN').innerHTML += `<h6>${
+      location.split("'")[1].split("'")[0]
+    }</h6>`;
+  });
 });
 
+// closing the link details view when in mobile view
 const close = document.querySelector('.icon.close-icon');
 
 close.onclick = function () {
@@ -12,83 +72,99 @@ close.onclick = function () {
   itemDetail.classList.remove('open');
 };
 
+// listening for click function on any of the links
 document.querySelector('.list--MAIN').onclick = function (e) {
-  console.log(e.target);
-  console.log(e.target.parentElement);
-  console.log(e.target.parentElement.parentElement);
-  console.log(e.target.parentElement.parentElement.parentElement);
-  console.log('');
-  if (
-    e.target.classList == 'bitlink-item--MAIN' ||
-    e.target.parentElement.classList == 'bitlink-item--MAIN' ||
-    e.target.parentElement.parentElement.classList == 'bitlink-item--MAIN' ||
-    e.target.parentElement.parentElement.parentElement.classList ==
-      'bitlink-item--MAIN'
+  if (e.target.classList == 'bitlink-item--MAIN') {
+    const activeDetail = document.querySelector('.bitlink-item--ACTIVE');
+    activeDetail.classList = 'bitlink-item--MAIN';
+    link = e.target;
+    link.classList = 'bitlink-item--ACTIVE';
+    updateView(link);
+  } else if (e.target.parentElement.classList == 'bitlink-item--MAIN') {
+    const activeDetail = document.querySelector('.bitlink-item--ACTIVE');
+    activeDetail.classList = 'bitlink-item--MAIN';
+    link = e.target.parentElement;
+    link.classList = 'bitlink-item--ACTIVE';
+    updateView(link);
+  } else if (
+    e.target.parentElement.parentElement.classList == 'bitlink-item--MAIN'
   ) {
-    alert('yay!');
+    const activeDetail = document.querySelector('.bitlink-item--ACTIVE');
+    activeDetail.classList = 'bitlink-item--MAIN';
+    link = e.target.parentElement.parentElement;
+    link.classList = 'bitlink-item--ACTIVE';
+    updateView(link);
+  } else if (
+    e.target.parentElement.parentElement.parentElement.classList ==
+    'bitlink-item--MAIN'
+  ) {
+    const activeDetail = document.querySelector('.bitlink-item--ACTIVE');
+    activeDetail.classList = 'bitlink-item--MAIN';
+    link = e.target.parentElement.parentElement.parentElement;
+    link.classList = 'bitlink-item--ACTIVE';
+    updateView(link);
   }
 };
 
-// console.log(
-//   document.querySelector('.bitlink-item--created-date').dataset.date_created
-// );
-// console.log(document.querySelector('.bitlink-item--title').dataset.author);
-// console.log(document.querySelector('.bitlink-item--title').dataset.title);
-// console.log(document.querySelector('.bitlink--MAIN').dataset.origin);
-// console.log(document.querySelector('.bitlink--MAIN').dataset.abbrefy);
-// console.log(document.querySelector('.click-count--MAIN').dataset.clicks);
-// console.log(document.querySelector('.click-count--MAIN').dataset.audience);
+function updateView(link) {
+  document.querySelector('.item-detail--created-date').textContent =
+    'CREATED ' +
+    link.querySelector('.bitlink-item--created-date').dataset.date_created;
 
-document.querySelector('.item-detail--created-date').textContent =
-  'CREATED ' +
-  document.querySelector('.bitlink-item--created-date').dataset.date_created;
+  document.querySelector('.item-detail--title').textContent =
+    link.querySelector('.bitlink-item--title').dataset.title;
 
-document.querySelector('.item-detail--title').textContent =
-  document.querySelector('.bitlink-item--title').dataset.title;
+  document.querySelector('.item-detail--url').textContent =
+    link.querySelector('.bitlink--MAIN').dataset.origin;
 
-document.querySelector('.item-detail--url').textContent =
-  document.querySelector('.bitlink--MAIN').dataset.origin;
+  document
+    .querySelector('.item-detail--url')
+    .setAttribute('href', link.querySelector('.bitlink--MAIN').dataset.origin);
 
-document
-  .querySelector('.item-detail--url')
-  .setAttribute(
-    'href',
-    document.querySelector('.bitlink--MAIN').dataset.origin
-  );
+  document.querySelector('.button--COPY').dataset.clipboard =
+    link.querySelector('.bitlink--MAIN').dataset.abbrefy;
 
-document.querySelector('.bitlink--detail--MAIN').textContent =
-  document.querySelector('.bitlink--MAIN').dataset.abbrefy;
+  document.querySelector('.bitlink--detail--MAIN').textContent =
+    link.querySelector('.bitlink--MAIN').dataset.abbrefy;
 
-document.querySelector('.bitlink--copy-tooltip').querySelector('input').value =
-  document.querySelector('.bitlink--MAIN').dataset.abbrefy;
+  document
+    .querySelector('.bitlink--copy-tooltip')
+    .querySelector('input').value =
+    link.querySelector('.bitlink--MAIN').dataset.abbrefy;
 
-document
-  .querySelector('.bitlink--detail--MAIN')
-  .setAttribute(
-    'title',
-    document.querySelector('.bitlink--MAIN').dataset.abbrefy
-  );
+  document
+    .querySelector('.bitlink--detail--MAIN')
+    .setAttribute(
+      'title',
+      link.querySelector('.bitlink--MAIN').dataset.abbrefy
+    );
 
-document
-  .querySelector('.bitlink--copyable-text')
-  .setAttribute(
-    'href',
-    document.querySelector('.bitlink--MAIN').dataset.abbrefy
-  );
+  document
+    .querySelector('.bitlink--copyable-text')
+    .setAttribute('href', link.querySelector('.bitlink--MAIN').dataset.abbrefy);
 
-document.querySelector('.info-wrapper--clicks-text').textContent =
-  document.querySelector('.click-count--MAIN').dataset.clicks;
+  document.querySelector('.info-wrapper--clicks-text').textContent =
+    link.querySelector('.click-count--MAIN').dataset.clicks;
 
-locations = [
-  ...document
-    .querySelector('.click-count--MAIN')
-    .dataset.audience.split('[')[1]
-    .split(']')[0]
-    .split(', '),
-];
+  if (link.querySelector('.click-count--MAIN').dataset.audience.length > 2) {
+    locations = [
+      ...link
+        .querySelector('.click-count--MAIN')
+        .dataset.audience.split('[')[1]
+        .split(']')[0]
+        .split(', '),
+    ];
 
-locations.forEach((location) => {
-  document.querySelector('.audience--locations---MAIN').innerHTML += `<h6>${
-    location.split("'")[1].split("'")[0]
-  }</h6>`;
-});
+    document.querySelector('.audience--locations---MAIN').innerHTML = '';
+
+    locations.forEach((location) => {
+      document.querySelector('.audience--locations---MAIN').innerHTML += `<h6>${
+        location.split("'")[1].split("'")[0]
+      }</h6>`;
+    });
+  } else {
+    document.querySelector('.audience--locations---MAIN').innerHTML = '';
+  }
+
+  // end of view update
+}
