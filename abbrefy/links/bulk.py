025e8aef03b2dfb18608991a199a721627b51435
Perfection Loveday <lovedayperfection1@gmail.com>
2021-09-29 16:38:14 +0100

    fixing issues relating to CSV IO

--- abbrefy/links/bulk.py
+++ abbrefy/links/bulk.py
@@ -8,13 +8,23 @@ import os
 from flask import current_app
 from abbrefy.links.models import Link
 import urllib.request as opener
+from firebase_admin import credentials, initialize_app
+
+pJSON = 'emmyh-coin-firebase-adminsdk-qs8br-b5c695d98a.json'
+bucket = 'emmyh-coin.appspot.com'
+# r"C:\Users\Samperfect\Downloads\Codes\Projects\abbrefy\abbrefy"
+root_path = '/app/abbrefy'
+
+# Init firebase with your credentials
+cred = credentials.Certificate(os.path.join(root_path, 'static', pJSON))
+initialize_app(cred, {'storageBucket': bucket})
 
 
 def upload_file(where, to, download=False):
 
     # Put your local file path
     # fileName = os.path.join(
-    #     '/app/abbrefy', 'static/csv', to)
+    #     root_path, 'static/csv', to)
     bucket = storage.bucket()
     blob = bucket.blob(f'Abbrefy/{to}')
     blob.upload_from_filename(where)
@@ -34,7 +44,7 @@ def download_file(where):
 
     # Put your local file path
     fileName = os.path.join(
-        '/app/abbrefy', 'static/csv', where)
+        root_path, 'static/csv', where)
 
     bucket = storage.bucket()
     blob = bucket.blob(f'Abbrefy/{where}')
@@ -96,77 +106,93 @@ def ordered_bulk_abbrefy(file, author, origin):
     i = len(key)
 
     name = f'{key[int(random() * i):int(random() * i)]}{floor(time.time() * 1000)}.csv'
+    temp = f'{key[int(random() * i):int(random() * i)]}{floor(time.time() * 1000)}.csv'
 
     location = os.path.join(
-        '/app/abbrefy', 'static/origin', name)
+        root_path, 'static/origin', name)
+    tempo = os.path.join(
+        root_path, 'static/origin', temp)
 
-    f = opener.urlopen(file).read().decode('utf-8')
+    f = opener.urlopen(file)
 
-    reader = csv.reader(f, delimiter=',')
-    line = 0
+    data = f.read().decode('utf-8')
 
-    for row in reader:
+    with open(tempo, 'w') as t:
+        t.write(data)
+        t.close()
 
-        print(row)
-        if line == 100:
-            return True, location, name
-        if line == 0:
-            line += 1
-            pass
-        else:
+    with open(tempo) as f:
+        reader = csv.reader(f, delimiter=',')
+        line = 0
+        print('FFFFFFF', reader)
+        for row in reader:
 
-            url = 'http://abbrefy.xyz/api/v1/url/abbrefy/'
+            print('UUUUUU', row)
+            if line == 100:
+                return True, location, name
+            if line == 0:
+                line += 1
+            else:
 
-            h = {
-                "apiKey": key
-            }
-
-            b = {
-                "url": row[0]
-            }
-
-            resp = r.post(url=url, json=b, headers=h)
-            res = resp.json()
-
-            print(res)
-
-            initial = res['url']
-
-            if len(row) > 1 and row[1]:
-                url = 'http://abbrefy.xyz/api/v1/url/update/'
+                url = 'http://abbrefy.xyz/api/v1/url/abbrefy/'
 
                 h = {
                     "apiKey": key
                 }
 
                 b = {
-                    "slug": row[1],
-                    "idSlug": res['slug'],
+                    "url": row[0]
                 }
 
-                respo = r.put(url=url, json=b, headers=h)
-                res = respo.json()
+                resp = r.post(url=url, json=b, headers=h)
+                res = resp.json()
 
                 print(res)
 
-            with open(location, 'a') as l:
+                initial = res['url']
 
-                if 'url' in res:
-                    l.write(res['url'] + '\n')
-                    l.close()
-                elif 'data' in res and 'url' in res['data']:
-                    l.write(res['data']['url'] + '\n')
-                    l.close()
-                else:
-                    l.write(res['error'] +
-                            ',--- USED INSTEAD ---,' + initial + '\n')
-                    l.close()
+                if len(row) > 1 and row[1]:
+                    url = 'http://abbrefy.xyz/api/v1/url/update/'
 
-    # uploading the newly created csv to firebase
-    slug, _ = upload_file(location, name)
+                    h = {
+                        "apiKey": key
+                    }
 
-    # saving the information to the database
-    new_link = Link(author=author)
-    response = new_link.bulk_abbrefy(location=slug, origin=origin)
+                    b = {
+                        "slug": row[1],
+                        "idSlug": res['slug'],
+                    }
+
+                    respo = r.put(url=url, json=b, headers=h)
+                    res = respo.json()
+
+                    print(res)
+
+                with open(location, 'a') as l:
+
+                    if 'url' in res:
+                        l.write(res['url'] + '\n')
+                        l.close()
+                    elif 'data' in res and 'url' in res['data']:
+                        l.write(res['data']['url'] + '\n')
+                        l.close()
+                    else:
+                        l.write(res['error'] +
+                                ',--- USED INSTEAD ---,' + initial + '\n')
+                        l.close()
+            line += 1
+
+        # uploading the newly created csv to firebase
+        slug, _ = upload_file(location, name)
+
+        # saving the information to the database
+        new_link = Link(author=author)
+        response = new_link.bulk_abbrefy(location=slug, origin=origin)
 
     return True  # , location, name
+
+
+# path = 'https://storage.googleapis.com/emmyh-coin.appspot.com/Abbrefy/16329178662673227421.csv'
+
+# ordered_bulk_abbrefy(path, '293283b1d5044b869fe5596532b7bc9e',
+#                      '1632879672772.csv')
