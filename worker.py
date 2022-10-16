c07cd38082359ca0903508a4fdc7c1f8addd11b3
Open Saucerer <lovedayperfection1@gmail.com>
2022-10-16 16:26:08 +0100

    feat: migrated from RedisToGo to RedisCloud

--- worker.py
+++ worker.py
@@ -3,15 +3,19 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
-listen = ['high', 'default', 'low']
+listen = ["high", "default", "low"]
 # instantiating redistogo
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+port = os.getenv("REDIS_PORT", 6379)
+username = os.getenv("REDIS_USER", "admin")
+password = os.getenv("REDIS_PASSWORD", "admin")
+host = os.getenv("REDIS_HOST", "localhost")
 
 # creating a connnection
-conn = redis.from_url(redis_url)
+conn = redis.Redis(host=host, port=port, password=password, username=username)
 
 # running when it is the main file
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
