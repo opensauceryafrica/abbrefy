74622bd83c86dc92e0aa3e6af2f996b9783504dd
Perfection Loveday <lovedayperfection1@gmail.com>
2023-05-17 11:20:44 +0100

    fix: updated models.py

--- abbrefy/links/models.py
+++ abbrefy/links/models.py
@@ -135,7 +135,7 @@ class Link:
     # abbrefy helper function
     def bulk_abbrefy(self, location, origin):
 
-        MONGO_URI = ''
+        MONGO_URI = os.environ.get("MONGO_URI", "")
 
         client = pymongo.MongoClient(MONGO_URI)
         db = client.abbrefy
